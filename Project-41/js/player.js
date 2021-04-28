d192f436af0b9c79da42c2250bbb7439a7be8744
AayanSimmy <75249461+AayanSimmy@users.noreply.github.com>
2021-04-28 11:12:44 +0530

    Add files via upload

---
+++ Project-41/js/player.js
@@ -0,0 +1,52 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+        this.score=0
+          // create the property for the score and intialize with zero
+    }
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score
+        });
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+
+    }
+
+    static getScore() {
+        var fruitCaughtRef = database.ref('fruitCaught');
+        fruitCaughtRef.on("value", (data) => {
+            fruitCaught = data.val();
+        })
+    }
+    updateScore(score) {
+        database.ref('/').update({
+            fruitsCaught: score
+        });
+    }
+
+}
